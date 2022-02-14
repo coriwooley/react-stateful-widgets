3a3e5b21228c8fa47b83009f44aa39d8104fcf37
Cori Wooley <wooleycori@gmail.com>
2022-02-14 07:38:28 -0800

    squares.js created 2 slices of state

--- src/components/Input.js
+++ src/components/Input.js
@@ -34,32 +34,41 @@ STEP 6:
   We need to add an extra prop to the <input /> element like so: value={inputValue}
 */
 
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 export default function Input() {
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState("");
 
-  const changeInput = evt => {
+  const changeInput = (evt) => {
     const { value } = evt.target;
-    setInputValue(value)
+    setInputValue(value);
   };
-  const reset = () => { 
-    setInputValue('')
+  const reset = () => {
+    setInputValue("");
   };
 
   const style = {
-    fontSize: '1.5em',
-    marginBottom: '0.3em',
-    color: inputValue.length > 10 ? 'crimson' : 'royalblue',
+    fontSize: "1.5em",
+    marginBottom: "0.3em",
+    color: inputValue.length > 10 ? "crimson" : "royalblue",
   };
 
   return (
-    <div className='widget-input container'>
+    <div className="widget-input container">
       <h2>Input</h2>
-      <div id='output' style={style}>{inputValue.toUpperCase()}</div> 
+      <div id="output" style={style}>
+        {inputValue.toUpperCase()}
+      </div>
       <div>
-        <input id='input' type='text' onChange={changeInput} value = {inputValue} />
-        <button id='resetInput' onClick={reset}>Reset</button>
+        <input
+          id="input"
+          type="text"
+          onChange={changeInput}
+          value={inputValue}
+        />
+        <button id="resetInput" onClick={reset}>
+          Reset
+        </button>
       </div>
     </div>
   );
